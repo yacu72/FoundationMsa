9ab76b9447f08fdb31d758b217553221cadbfe38
Mauro <mauro@mojahmedia.net>
2018-08-27 20:06:14 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "off-canvas-wrap" div and all content after.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+?>
+
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-grid">
+            <?php dynamic_sidebar( 'footer-widgets' ); ?>
+        </div>
+    </div>
+</footer>
+
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+	</div><!-- Close off-canvas content -->
+<?php endif; ?>
+
+<?php wp_footer(); ?>
+
+<script>
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 130) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
+
+
+</script>
+
+</body>
+</html>
