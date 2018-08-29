c09e639fb989d85d63874d754fbbdd2d354138c9
Mauro <mauro@mojahmedia.net>
2018-08-29 15:04:24 -0300

    adding blocks to blogs template

--- page-templates/blogs-tepmplate1.php
+++ page-templates/blogs-tepmplate1.php
@@ -69,7 +69,7 @@ get_header(); ?>
 					<div class="post-wrap">
 
 						<div class="item" >
-							<article class="grid-post">
+							<article class="grid-post post-style-1">
 							<div class="post-header">
 								<div class="post-thumb">
 									<a href=""><img src="https://unsplash.it/501/?random"></a>
@@ -87,7 +87,7 @@ get_header(); ?>
 						</div>
 
 						<div class="item" >
-							<article class="grid-post">
+							<article class="grid-post post-style-1">
 							<div class="post-header">
 								<div class="post-thumb">
 									<a href=""><img src="https://unsplash.it/502/?random"></a>
@@ -105,7 +105,7 @@ get_header(); ?>
 						</div>
 
 						<div class="item" >
-							<article class="grid-post">
+							<article class="grid-post post-style-1">
 							<div class="post-header">
 								<div class="post-thumb">
 									<a href=""><img src="https://unsplash.it/503/?random"></a>
@@ -125,8 +125,124 @@ get_header(); ?>
 					</div>
 				</div>
 			</div>
-
 		</section>
+
+		<section class="section call-to-action gird-x">
+			<div class="section-head">
+
+			</div>
+
+			<div class="section-content cell text-center">
+				<span class="h3">September Registration Call, limited seats.</span> <a class="button large round">Apply Now</a>
+			</div>
+		</section>	
+
+
+		<section class="grid-x grid-margin-x">
+
+			<div class="section-column cell medium-8">
+				<div class="section-column-inner  grid-margin-x grid-x">
+
+					<div class="column-container cell medium-6">
+
+						<div class="section-head">
+							<h4 class="title">
+								<a href="">Studying Overseas</a>
+							</h4>
+						</div>
+
+						<article class="grid-post post-style-1">
+							<div class="post-header">
+								<div class="post-thumb">
+									<a href=""><img src="https://unsplash.it/506/?random"></a>
+								</div>
+								<div class="meta-title">
+									<h2 class="post-title"><a href="">Romanian University</a></h2>
+									<span class="post-author">By <a href="">Shae Klark</a></span>
+									<span class="post-date">August 12, 2009</span>
+								</div>
+							</div>
+							<div class="post-content">
+								<p>More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous…</p>
+							</div>
+						</article>
+					</div>
+
+					<div class="column-container cell medium-6">
+
+						<div class="section-head">
+							<h4 class="title">
+								<a href="">Romanian Life</a>
+							</h4>
+						</div>
+
+						<article class="grid-post post-style-1">
+							<div class="post-header">
+								<div class="post-thumb">
+									<a href=""><img src="https://unsplash.it/507/?random"></a>
+								</div>
+								<div class="meta-title">
+									<h2 class="post-title"><a href="">Romanian University</a></h2>
+									<span class="post-author">By <a href="">Shae Klark</a></span>
+									<span class="post-date">August 12, 2009</span>
+								</div>
+							</div>
+							<div class="post-content">
+								<p>More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous…</p>
+							</div>
+						</article>
+					</div>					
+
+				</div>
+
+			</div>
+			<div class="section-column side-column medium-4">
+
+				<div class="section-head section-head-1">
+					<h4 class="title">
+						Top Schools
+					</h4>
+				</div>
+
+				<div class="section-content items-style-1">
+
+					<div class="item " >
+						<a href="">
+							<div class="item-thumb">
+								<img src="https://unsplash.it/601/200?random" >
+							</div>
+							<span class="label">Iasi Med School</span>
+						</a>
+					</div>
+
+					<div class="item ">
+						<a href="">
+							<div class="item-thumb">
+								<img src="https://unsplash.it/603/200?random" >
+							</div>
+							<span class="label">Cluj Med School</span>
+						</a>
+					</div>					
+
+				</div>
+
+				<div class="section-head section-head-1">
+					<h4 class="title">
+						Follow Us
+					</h4>
+				</div>	
+
+				<div class="social-icons">
+					<a class="social-link" href="">
+						<i class="fa fa-facebook"></i>
+					</a>
+					<a class="social-link" href="">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>			
+				
+			</div>
+		</section>	
 
 			<!--<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
